e3c968e24dd12ac6263e21510c27603cc4dde3ed
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-09-21 11:05:09 +0200

    added IP, call count and env info

--- data/data.go
+++ data/data.go
@@ -1,14 +1,18 @@
 package data
 
 import (
+	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 type RequestData struct {
 	Host        map[string]string `json:"host"`
+	Environment map[string]string `json:"env"`
 	Headers     map[string]string `json:"headers"`
 	QueryParams map[string]string `json:"query"`
 	Caller      Caller            `json:"caller"`
@@ -28,9 +32,16 @@ type Caller struct {
 	IP string `json:"ip"`
 }
 
+var localIP net.IP
+var calls int
+
 func GetCallData(c echo.Context) RequestData {
+
+	calls++
+
 	data := RequestData{}
 
+	addEnvironmentInfo(&data)
 	addHostInfo(&data)
 
 	data.GetHeaders(c.Request().Header)
@@ -42,13 +53,34 @@ func GetCallData(c echo.Context) RequestData {
 	return data
 }
 
+func addEnvironmentInfo(req *RequestData) {
+	env := map[string]string{}
+	req.Environment = env
+
+	env["name"] = os.Getenv("INSTANCE_NAME")
+	env["calls"] = strconv.Itoa(calls)
+}
+
 func addHostInfo(req *RequestData) {
+
+	if localIP == nil {
+		localIP = getOutboundIP()
+	}
 
 	host := map[string]string{}
 	req.Host = host
 	host["hostname"], _ = os.Hostname()
+	host["ip"] = localIP.String()
+}
 
-	// conn, _ := net.Dial("ip:icmp", "google.com")
-	// host["ip"] = conn.LocalAddr().String()
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
